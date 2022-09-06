9e05dfa30dee1e8d6d093de994782c11e69c0dba
Harry-58 <56926733+Harry-58@users.noreply.github.com>
2022-09-06 17:54:52 +0200

    Anpassung wegen ESP8266

--- myMacros.h
+++ myMacros.h
@@ -17,7 +17,11 @@
 
 // 01234567890
 // Jan 11 2020   -->  11 Jan 2020
-#define BUILDDATE (char const[]){ __DATE__[4], __DATE__[5], ' ', __DATE__[0] , __DATE__[1], __DATE__[2] ,' ', __DATE__ [7], __DATE__ [8], __DATE__ [9], __DATE__ [10],'\0' }
+#ifndef ESP8266
+#define BUILDDATE ( char const[]){ __DATE__[4], __DATE__[5], ' ', __DATE__[0] , __DATE__[1], __DATE__[2] ,' ', __DATE__ [7], __DATE__ [8], __DATE__ [9], __DATE__ [10],'\0' }
+#else   //TODO: bei ESP8266 kommt error: taking address of temporary array
+#define BUILDDATE __DATE__
+#endif
 
 //siehe auch  https://www.mikrocontroller.net/topic/315552
 #endif
