198f53effea22ec39820ef279ea0bf1fc2061f16
Harry-58 <56926733+Harry-58@users.noreply.github.com>
2022-09-18 10:24:46 +0200

    Namen geändert (alles mit zwei Underlines )

--- myDebug/myDebug.h
+++ myDebug/myDebug.h
@@ -22,42 +22,58 @@
 
    #ifdef DEBUG_EIN
       #include <Streaming.h>
-      #ifndef DEBUG_ZIEL
-         #define DEBUG_ZIEL Serial   // Standardziel für Debugmeldungen
+      #ifndef DEBUG__ZIEL
+        #define DEBUG__ZIEL Serial   // Standardziel für Debugmeldungen
       #endif
+      #ifndef DEBUG__
+        #define DEBUG__(d) {d;}                       // ganzen Block nur bei DEBUG_EIN ausführen
+      #endif
+      #ifndef DEBUG__PRINT
+        #define DEBUG__PRINT(x)    DEBUG__ZIEL.print(x)
+      #endif
+      #ifndef DEBUG__PRINTLN
+        #define DEBUG__PRINTLN(x)  DEBUG__ZIEL.println(x)
+      #endif
+      #ifndef DEBUG__PRINTF
+        #if defined(__AVR__)       // Arduino hat kein printf deshalb über snprintf mit Buffer
+                                  // https://www.e-tinkers.com/2020/01/do-you-know-arduino-sprintf-and-floating-point/
+          extern char myDebugBuffer[_MYDEBUGPRINTFBUFFER_SIZE];
+          #define DEBUG_PRINTF(...) {  snprintf(myDebugBuffer,sizeof(myDebugBuffer),__VA_ARGS__); DEBUG_ZIEL.print(myDebugBuffer);}
+        #else
+          #define DEBUG__PRINTF(...) DEBUG__ZIEL.printf( __VA_ARGS__ )
+        #endif
+      #endif
+      #ifndef DEBUG__INFO
+        #define DEBUG__INFO(x)     DEBUG__ZIEL <<"**** F:"<<__FUNCTION__ <<" L:"<< __LINE__ <<" # "<< x << endl   // Funtionsname, Zeilennummer und Parameter anzeigen
+      #endif
+      #ifndef DEBUG__VAR
+        #define DEBUG__VAR(x)      DEBUG__ZIEL << (#x) << " >" <<(x) << "<" << endl                               // Variablenname und Wert anzeigen
+      #endif
+      #ifndef DEBUG__SHOW
+        #define DEBUG__SHOW(x)                   (#x) << " >" <<(x) << "<"                                       // Variablenname und Wert innerhalb anderen (Debug)-Prints anzuzeigen
+      #endif
+        #ifndef ESP32
+          #define ARDUHAL_LOG_COLOR_E
+          #define ARDUHAL_LOG_COLOR_W
+          #define ARDUHAL_LOG_COLOR_I
+          #define ARDUHAL_LOG_COLOR_D
+          #define ARDUHAL_LOG_COLOR_V
+          #define ARDUHAL_LOG_RESET_COLOR
+          #define ARDUHAL_LOG_FORMAT(letter, format)  ARDUHAL_LOG_COLOR_ ## letter "[" #letter "][%s:%u] %s(): " format ARDUHAL_LOG_RESET_COLOR "\r\n", __FILE__, __LINE__, __FUNCTION__
 
-      #define DEBUG(d) {d;}                                                                                   // ganzen Block nur bei DEBUG_EIN ausführen
-      #define DEBUG_PRINT(x)    DEBUG_ZIEL.print(x)
-      #define DEBUG_PRINTLN(x)  DEBUG_ZIEL.println(x)
-      #if defined(__AVR__)       // Arduino hat kein printf deshalb über snprintf mit Buffer
-                                 // https://www.e-tinkers.com/2020/01/do-you-know-arduino-sprintf-and-floating-point/
-        extern char myDebugBuffer[_MYDEBUGPRINTFBUFFER_SIZE];
-        #define DEBUG_PRINTF(...) {  snprintf(myDebugBuffer,sizeof(myDebugBuffer),__VA_ARGS__); DEBUG_ZIEL.print(myDebugBuffer);}
-      #else
-        #define DEBUG_PRINTF(...) DEBUG_ZIEL.printf( __VA_ARGS__ )
       #endif
-      #define DEBUG_INFO(x)     DEBUG_ZIEL <<"**** F:"<<__FUNCTION__ <<" L:"<< __LINE__ <<" # "<< x << endl   // Funtionsname, Zeilennummer und Parameter anzeigen
-      #define DEBUG_VAR(x)      DEBUG_ZIEL << (#x) << " >" <<(x) << "<" << endl                               // Variablenname und Wert anzeigen
-      #define DEBUG_SHOW(x)                   (#x) << " >" <<(x) << "<"                                       // Variablenname und Wert innerhalb anderen (Debug)-Prints anzuzeigen
-      #ifndef ESP32
-        #define ARDUHAL_LOG_COLOR_E
-        #define ARDUHAL_LOG_COLOR_W
-        #define ARDUHAL_LOG_COLOR_I
-        #define ARDUHAL_LOG_COLOR_D
-        #define ARDUHAL_LOG_COLOR_V
-        #define ARDUHAL_LOG_RESET_COLOR
-        #define ARDUHAL_LOG_FORMAT(letter, format)  ARDUHAL_LOG_COLOR_ ## letter "[" #letter "][%s:%u] %s(): " format ARDUHAL_LOG_RESET_COLOR "\r\n", __FILE__, __LINE__, __FUNCTION__
+      #ifndef LOG__E
+        #define LOG__E(format, ...)DEBUG_PRINTF(ARDUHAL_LOG_FORMAT(E, format), ##__VA_ARGS__)
       #endif
-      #define LOG_E(format, ...)DEBUG_PRINTF(ARDUHAL_LOG_FORMAT(E, format), ##__VA_ARGS__)
-   #else
-      #define DEBUG(d)  {}
-      #define DEBUG_PRINT(x)
-      #define DEBUG_PRINTLN(x)
-      #define DEBUG_PRINTF(...)
-      #define DEBUG_INFO(x)
-      #define DEBUG_VAR(x)
-      #define DEBUG_SHOW(x)
-      #define LOG_E(format, ...)
+   #else  // Debug ist ausgeschaltet
+      #define DEBUG__(d)  {}
+      #define DEBUG__PRINT(x)
+      #define DEBUG__PRINTLN(x)
+      #define DEBUG__PRINTF(...)
+      #define DEBUG__INFO(x)
+      #define DEBUG__VAR(x)
+      #define DEBUG__SHOW(x)
+      #define LOG__E(format, ...)
    #endif
 
 #endif
